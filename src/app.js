712e4a9ec2b877f640cbc18ca942f6ae62c649db
Isa Omarzada <isaomarzada@yahoo.com>
2019-04-19 16:33:52 -0500

    assignment intro to express complete

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 
-const routeConfig = require("./config/route-config.js");
+app.use("/", (req, res, next) => {
+   res.send("polo")
+ });
 
- routeConfig.init(app);
 
 
 module.exports = app;
