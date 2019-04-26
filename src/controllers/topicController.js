1737f198577ddc1182db260785b582825993e88e
Isa Omarzada <isaomarzada@yahoo.com>
2019-04-26 15:08:41 -0500

    fix

--- src/controllers/topicController.js
+++ src/controllers/topicController.js
@@ -3,7 +3,13 @@ const topicQueries = require("../db/queries.topics.js");
 module.exports = {
   index(req, res, next){
     topicQueries.getAllTopics((err, topics) => {
-      })
+    if(err) {
+      res.redirect(500, "static/index");
+    }
+    else {
+      res.render('topics/index', {topics});
+       }
+    })
   },
   new(req, res, next){
       res.render("topics/new");
