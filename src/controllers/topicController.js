6ab28dabe919c6791a9459bcfdc8036391378b9d
Isa Omarzada <isaomarzada@yahoo.com>
2019-04-26 11:48:49 -0500

    6 failures

--- src/controllers/topicController.js
+++ src/controllers/topicController.js
@@ -3,15 +3,6 @@ const topicQueries = require("../db/queries.topics.js");
 module.exports = {
   index(req, res, next){
     topicQueries.getAllTopics((err, topics) => {
-      Topic.hasMany(models.Banner, {
-      foreignKey: "topicId",
-      as: "banners",
-      });
-        if(err){
-          res.redirect(500, "static/index");
-        } else {
-          res.render("topics/index", {topics});
-        }
       })
   },
   new(req, res, next){
