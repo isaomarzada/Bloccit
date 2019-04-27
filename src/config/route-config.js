9393a6dd2e643249971c7673f746af5bc752101b
Isa Omarzada <isaomarzada@yahoo.com>
2019-04-26 20:59:57 -0500

    advertisement

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -2,6 +2,8 @@ module.exports = {
   init(app){
     const staticRoutes = require("../routes/static");
     const topicRoutes = require("../routes/topics");
+    const advertisementRoutes = require("../routes/advertisement");
+    app.use(advertisementRoutes);
     app.use(staticRoutes);
     app.use(topicRoutes);
   }
